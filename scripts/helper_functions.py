508d917a810d0b7454133a2dc83511a13cf97162
DieBirke <38507012+DieBirke@users.noreply.github.com>
2022-06-30 10:04:22 +0200

    bl ratio fixed

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -50,6 +50,8 @@ def get_eng_values(df, feat_list):
             f0 = get_bl_ratios_of(entry[9:-1])
             f1 = df.columns[1]
             df_tmp = add_ratio_of(df_bl, f0, f1, 'tmp')
+            print('df_tmp comumns')
+            print(df_tmp.columns)
             ret.append(df_tmp['tmp'].values[0])
     return ret
     
